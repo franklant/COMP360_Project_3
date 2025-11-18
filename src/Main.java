7fdf6779b975804558cdf1e0aab274de758f7ab4
franklant <frankboss349@gmail.com>
2025-11-18 12:19:49 -0500

    added WestTheaterFullException

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,22 @@
-// Franklan Taylor
-// COMP360-001
-// 11/11/2025
-// A Seat Reservation program where the user can select a date, time, seat and either reserve that seat
+// Name: Franklan Taylor
+// Date: 11/11/2025
+// Course: COMP360-001
+// Description: A Seat Reservation program where the user can select a date, time, seat and either reserve that seat
 // or return an exception saying the seat is full.
 
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("YEAHHHHHHHHH");
+    public static void main(String[] args)
+    {
+        Main app = new Main();
+        app.throwException();
+    }
+
+    /**
+     * <p>Throws the  {@link WestTheaterFullException} as a test.</p>
+     */
+    public void throwException()
+    {
+        throw new WestTheaterFullException();
     }
 }
---
+++ src/WestTheaterFullException.java
@@ -0,0 +1,30 @@
+// Name: Franklan Taylor
+// Date:
+// Course: COMP360-001
+// Description:
+
+
+
+
+/**
+ * <p>Thrown to indicate that the theater no longer has seats available to the customer.</p>
+ * @since 0.0
+ */
+public class WestTheaterFullException extends IllegalArgumentException{
+    /**
+     * <p>Constructs an {@code WestTheaterFullException} with a detailed message</p>
+     */
+    public WestTheaterFullException()
+    {
+        super("No Seat Available.");
+    }
+
+    /**
+    * Constructs an {@code WestTheaterFullException} with a <i>CUSTOM</i> detailed message
+    * @param message the message sent to the console once the exception is thrown.
+    */
+    public WestTheaterFullException(String message)
+    {
+        super(message);
+    }
+}
