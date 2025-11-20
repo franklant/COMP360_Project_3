51ca4fbc5f63b58a59b472d60c68ef429e5d5819
franklant <frankboss349@gmail.com>
2025-11-20 12:11:02 -0500

    added new exceptions, add movies, show movie info

---
+++ src/WestTheaterReservationSystem.java
@@ -0,0 +1,17 @@
+
+
+/**
+ * <p>description...</p>
+ * @param param description...
+ * @return value description
+ */
+
+public class WestTheaterReservationSystem {
+    protected final int MAX_COLUMNS = 20;
+    protected final int MAX_ROWS = 10;
+    protected final int MAX_PEOPLE = MAX_COLUMNS * MAX_ROWS;
+    public WestTheaterReservationSystem()
+    {
+
+    }
+}
